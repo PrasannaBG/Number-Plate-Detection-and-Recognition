c27d0f1b0ab1560db39a9ac2a0c56e78ae344a6e
PrasannaBG <88681746+PrasannaBG@users.noreply.github.com>
2021-08-10 01:50:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pytesseract
 import numpy as np
-from PIL import Image # (Python image library)
+from PIL import Image 
+pytesseract.pytesseract.tesseract_cmd = './.apt/usr/bin/tesseract'
 st.title("OCR - Optical Character Recognition")
-img = st.sidebar.file_uploader("Choose an image") # sidebar makes the drop files in left hand side
+img = st.sidebar.file_uploader("Choose an image") 
 if img is not None:
   img_read = Image.open(img)
   img_new = np.array(img_read)
